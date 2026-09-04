8c1c7209bffe2d575409ff6331422a130954f6f3
mvinduwara <mvinduwara@gmail.com>
2026-09-04 20:15:58 +0530

    Create LeaveRequestRepository.java

---
+++ src/main/java/com/retailhr/ems/repository/LeaveRequestRepository.java
@@ -0,0 +1,44 @@
+package com.retailhr.ems.repository;
+
+import com.retailhr.ems.model.entity.Employee;
+import com.retailhr.ems.model.entity.LeaveRequest;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class LeaveRequestRepository extends GenericRepository<LeaveRequest, Integer> {
+
+    public LeaveRequestRepository() {
+        super(LeaveRequest.class);
+    }
+
+    public List<LeaveRequest> findByEmployee(Employee employee) {
+        try (EntityManager em = entityManager()) {
+            return em.createQuery(
+                            "SELECT l FROM LeaveRequest l WHERE l.employee = :employee " +
+                                    "ORDER BY l.createdAt DESC",
+                            LeaveRequest.class)
+                    .setParameter("employee", employee)
+                    .getResultList();
+        }
+    }
+
+    public List<LeaveRequest> findByStatus(LeaveRequest.Status status) {
+        try (EntityManager em = entityManager()) {
+            return em.createQuery(
+                            "SELECT l FROM LeaveRequest l WHERE l.status = :status " +
+                                    "ORDER BY l.createdAt ASC",
+                            LeaveRequest.class)
+                    .setParameter("status", status)
+                    .getResultList();
+        }
+    }
+
+    public List<LeaveRequest> findPendingForApproval() {
+        return findByStatus(LeaveRequest.Status.PENDING);
+    }
+
+    public List<LeaveRequest> findAll() {
+        return super.findAll();
+    }
+}
