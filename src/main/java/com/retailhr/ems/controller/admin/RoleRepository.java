9ee01c8a7c499c59f27a414c3c6f0745ed53f5ec
mvinduwara <mvinduwara@gmail.com>
2026-09-04 20:14:58 +0530

    Create RoleRepository.java

---
+++ src/main/java/com/retailhr/ems/controller/admin/RoleRepository.java
@@ -0,0 +1,27 @@
+package com.retailhr.ems.controller.admin;
+
+import com.retailhr.ems.model.entity.Role;
+import com.retailhr.ems.repository.GenericRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+
+import java.util.Optional;
+
+public class RoleRepository extends GenericRepository<Role, Integer> {
+
+    public RoleRepository() {
+        super(Role.class);
+    }
+
+    public Optional<Role> findByName(String roleName) {
+        try (EntityManager em = entityManager()) {
+            Role role = em.createQuery(
+                            "SELECT r FROM Role r WHERE r.roleName = :name", Role.class)
+                    .setParameter("name", roleName)
+                    .getSingleResult();
+            return Optional.of(role);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+}
